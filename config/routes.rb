6bd8a233e6e56e017fc101678039d5a0aeb8badd
Tushita(2016831026) <kazi26@student.sust.edu>
2019-07-19 15:49:39 +0600

    update balance

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,8 @@ Rails.application.routes.draw do
   post '/manager_panel',            to: 'managers#remove_admin'
   get  '/add_expense',              to: 'expenses#new'
   post '/add_expense',              to: 'expenses#create'
-  get 'show_expense',               to: 'expenses#show'     
+  get '/show_expense',              to: 'expenses#show'
+  get '/update_balance',            to: 'users#balance_update'
 
 
   get  '/create_new_lunch_menu',    to: 'lunch_managers#new_meal'
